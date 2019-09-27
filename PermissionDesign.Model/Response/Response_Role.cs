fe371060e50045c3c42c8e88bd17fe7646c1326a
zj f <1508347118@qq.com>
2019-09-27 21:35:11 +0800

    添加角色查询Response

---
+++ PermissionDesign.Model/Response/Response_Role.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PermissionDesign.Model.Response
+{
+   public class Response_Role
+    {
+        public int Id { get; set; }
+        public string RoleEncoding { get; set; }
+        public string RoleName { get; set; }
+        public string RoleDesc { get; set; }
+        public DateTime AddDate { get; set; }
+        public string Remark { get; set; }
+    }
+}
