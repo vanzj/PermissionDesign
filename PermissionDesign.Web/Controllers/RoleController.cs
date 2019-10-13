3efc5c17cd98d1767d74b4edcf2314e901747c4c
zj f <1508347118@qq.com>
2019-10-13 16:41:14 +0800

    添加角色控制器编写

--- PermissionDesign.Web/Controllers/RoleController.cs
+++ PermissionDesign.Web/Controllers/RoleController.cs
@@ -7,6 +7,7 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using PermissionDesign.Model;
 using PermissionDesign.Model.Entity;
+using PermissionDesign.Model.Request;
 using PermissionDesign.Model.Response;
 using PermissionDesign.Service;
 using PermissionDesign.Tool;
@@ -35,7 +36,7 @@ namespace PermissionDesign.Web.Controllers
         /// <param name="startTime">创建时间</param>
         /// <param name="endTime">时间调节</param>
         /// <returns></returns>
-        public JsonResult GetRolesByPage(int pageSize,int pageCount, string  search_des,DateTime? startTime,DateTime? endTime)
+        public JsonResult GetRolesByPage(int pageSize, int pageCount, string search_des, DateTime? startTime, DateTime? endTime)
         {
             var total = 0;
             var wheres = new List<Expression<Func<Role, bool>>>();
@@ -45,17 +46,55 @@ namespace PermissionDesign.Web.Controllers
             }
             if (startTime != null)
             {
-             
-                wheres.Add(c => c.AddDate >=  startTime);
+                wheres.Add(c => c.AddDate >= startTime);
             }
             if (endTime != null)
             {
-                wheres.Add(c => c.AddDate <= endTime );
+                wheres.Add(c => c.AddDate <= endTime);
             }
-       var result =     _roleService.GetRolesByPage(wheres, out total, 0, pageSize, pageCount);
+            var result = _roleService.GetRolesByPage(wheres, out total, 0, pageSize, pageCount);
             if (result.code == 200)
                 return Json(new { total = total, rows = result.data as List<Response_Role> });
-            return Json(new { total =0,row=""});
+            return Json(new { total = 0, row = "" });
         }
+
+        /// <summary>
+        /// 角色名称是否存在
+        /// </summary>
+        /// <param name="roleName"></param>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        public JsonResult ExistsRoleName(string roleName, int id = 0)
+        {
+            var temp = _roleService.ExistsRole(c => c.RoleName != roleName && c.Id == id);
+            return Json(temp);
+        }
+        /// <summary>
+        /// 角色编码是否存在
+        /// </summary>
+        /// <param name="roleEncoding"></param>
+        /// <returns></returns>
+        public JsonResult ExistsRoleEncoding(string roleEncoding)
+        {
+            var temp = _roleService.ExistsRole(c => c.RoleEncoding == roleEncoding);
+            return Json(temp);
+        }
+        /// <summary>
+        /// 添加角色
+        /// </summary>
+        /// <param name="role"></param>
+        /// <returns></returns>
+        public ActionResult AddRole(Request_AddRole role)
+        {
+            ResponseModel response = _roleService.AddRole(role);
+            if (response.code ==200)
+            {
+                Response.Redirect("/Wrong/Index?wrongContent=" + response.result+"&returnUrl="+"/Role/Index");
+                return null;
+            }
+
+            return View("Index");
+        }
+
     }
 }
